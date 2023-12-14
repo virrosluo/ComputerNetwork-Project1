a02c70c1b69c4faa219c457b809979dd7cbaf40c
treerspeaking <93274119+treerspeaking@users.noreply.github.com>
2023-12-14 13:42:34 +0700

    Update main.jsx

--- ServerUI/src/main.jsx
+++ ServerUI/src/main.jsx
@@ -4,9 +4,9 @@ import App from './App.jsx'
 import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <SnackbarProvider maxSnack={3}>
     <App />
     </SnackbarProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
